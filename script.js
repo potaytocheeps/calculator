6c30a00e5fb6ca9a5632f4f4e7a1edd2a4bf818d
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2023-10-24 21:49:29 -0700

    Add function to invoke any of the basic math operations

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function divide(numberA, numberB)
 {
     return numberA / numberB;
 }
+
+function operate(operator, numberA, numberB)
+{
+    let result = null;
+
+    switch(operator)
+    {
+        case '+':
+            result = add(numberA, numberB);
+            break;
+        case '-':
+            result = subtract(numberA, numberB);
+            break;
+        case '*':
+            result = multiply(numberA, numberB);
+            break;
+        case '/':
+            result = divide(numberA, numberB);
+            break;
+    }
+
+    return result;
+}
