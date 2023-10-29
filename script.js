d7caca9a8612045650734091e22faeb8a10a9a88
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2023-10-29 15:46:12 -0700

    Refactor code for better readability
    - Extracted the code for adding functionality to the different buttons
      into their own functions. Prior to this change, all of the code was
      in one function, creating a really long function that became
      confusing to read.
    
    - Created an object containing the DOM elements that need to be
      referenced in multiple functions for easier manipulation. Also added
      the calculationIsComplete variable to the object, since it is
      referenced in and needs to be modified by multiple different
      functions, and this is the only way to accomplish that.
    
    - Also changed the initial value of the calculationIsComplete variable
      from true to false. The previous initial value was a mistake that
      had gone unnoticed until now.

--- script.js
+++ script.js
@@ -35,6 +35,147 @@ function enableOperatorButtons(operatorButtons)
     });
 }
 
+function addFunctionalityToNumberButtons(calculatorElements, displayMessage)
+{
+    const numberButtons = document.querySelectorAll('.number-button');
+    let displayMessageArray = null;
+
+    numberButtons.forEach((numberButton) => {
+        numberButton.addEventListener('click', (event) => {
+            if (calculatorElements.calculationIsComplete)
+            {
+                displayMessage.textContent = '';
+                calculatorElements.calculationIsComplete = false;
+            }
+
+            displayMessage.textContent += event.target.textContent;
+
+            displayMessageArray = displayMessage.textContent.trimEnd().split(' ');
+
+            enableOperatorButtons(calculatorElements.operatorButtons);
+
+            // Only enable equals button when an operation contains both operands and operator
+            if (displayMessageArray.length >= 3)
+            {
+                calculatorElements.equalsButton.disabled = false;
+            }
+        });
+    });
+}
+
+function addFunctionalityToOperatorButtons(calculatorElements, displayMessage)
+{
+    let displayMessageArray = null;
+
+    calculatorElements.operatorButtons.forEach((operatorButton) => {
+        operatorButton.disabled = true;
+
+        operatorButton.addEventListener('click', (event) => {
+            displayMessage.textContent += ' ' + event.target.textContent + ' ';
+
+            disableOperatorButtons(calculatorElements.operatorButtons);
+
+            displayMessageArray = displayMessage.textContent.trimEnd().split(' ');
+
+            // If the user has entered more than one operator in a calculation, simulate
+            // the clicking of the "=" button, and use the result of the calculation as the
+            // first operand of a new calculation, adding to it the second operator entered
+            if (displayMessageArray.length > 3)
+            {
+                calculatorElements.equalsButton.dispatchEvent(new Event('click'));
+
+                // Only add the operator to the display if the display does not contain a letter.
+                // This prevents the user from creating a calculation after they try to divide
+                // by 0, and a message appears on the display
+                if (!displayMessage.textContent.match(/[a-z]/))
+                {
+                    displayMessage.textContent += ' ' + event.target.textContent + ' ';
+                    calculatorElements.calculationIsComplete = false;
+                }
+            }
+            else
+            {
+                calculatorElements.calculationIsComplete = false;
+            }
+
+            // If an operator is used, that means the user is currently entering the second
+            // operand, so the decimal button should be enabled again if it had been previously
+            // disabled with the first operand
+            calculatorElements.decimalButton.disabled = false;
+        });
+    });
+}
+
+function addFunctionalityToEqualsButton(calculatorElements, displayMessage)
+{
+    calculatorElements.equalsButton.addEventListener('click', () => {
+        const displayArray = displayMessage.textContent.split(' ');
+        const firstOperand = Number(displayArray[0]);
+        const operator = displayArray[1];
+        const secondOperand = Number(displayArray[2]);
+
+        displayMessage.textContent = '';
+
+        displayMessage.textContent = calculator.operate(operator, firstOperand, secondOperand);
+
+        calculatorElements.equalsButton.disabled = true;
+        calculatorElements.calculationIsComplete = true;
+        calculatorElements.decimalButton.disabled = false;
+
+        // If the user previously tried to divide by 0, disable the operator buttons to prevent
+        // an error in calculation
+        if (displayMessage.textContent.match(/[a-z]/))
+        {
+            disableOperatorButtons(calculatorElements.operatorButtons);
+        }
+        // Round answers with long decimals up to 4 decimal places or 4 significant figures
+        // to avoid overflow of the display
+        else if (!Number.isInteger(Number(displayMessage.textContent)))
+        {
+            if (Number(displayMessage.textContent) > 1)
+            {
+                displayMessage.textContent =
+                        Number(displayMessage.textContent).toFixed(4) * 1;
+            }
+            else
+            {
+                displayMessage.textContent =
+                        Number(displayMessage.textContent).toPrecision(4) * 1;
+            }
+        }
+    });
+}
+
+function addFunctionalityToClearButton(calculatorElements, displayMessage)
+{
+    const clearButton = document.querySelector('.button-clear');
+
+    clearButton.addEventListener('click', () => {
+        displayMessage.textContent = '';
+        disableOperatorButtons(calculatorElements.operatorButtons);
+        calculatorElements.equalsButton.disabled = true;
+        calculatorElements.decimalButton.disabled = false;
+    });
+}
+
+function addFunctionalityToDecimalButton(calculatorElements, displayMessage)
+{
+    calculatorElements.decimalButton.addEventListener('click', (event) => {
+        if (calculatorElements.calculationIsComplete)
+        {
+            displayMessage.textContent = '';
+            calculatorElements.calculationIsComplete = false;
+        }
+
+        displayMessage.textContent += event.target.textContent;
+
+        // Only allow one decimal point to be used per operand
+        calculatorElements.decimalButton.disabled = true;
+
+        disableOperatorButtons(calculatorElements.operatorButtons);
+    });
+}
+
 function deleteCharactersFromDisplay(displayString, displayMessage, decimalButton)
 {
     if (displayString.endsWith(' '))
@@ -83,140 +224,14 @@ function enableOrDisableButtons(displayArray, operatorButtons, decimalButton, eq
     }
 }
 
-function addFunctionalityToButtons()
+function addFunctionalityToBackspaceButton(calculatorElements, displayMessage)
 {
-    const numberButtons = document.querySelectorAll('.number-button');
-    const displayMessage = document.querySelector('.display-message');
-    const operatorButtons = document.querySelectorAll('.operator-button');
-    const equalsButton = document.querySelector('.button-equals');
-    const clearButton = document.querySelector('.button-clear');
-    const decimalButton = document.querySelector('.button-decimal');
     const backspaceButton = document.querySelector('.button-backspace');
-    let displayMessageArray = null;
-    let calculationIsComplete = true;
-    equalsButton.disabled = true;
-
-    numberButtons.forEach((numberButton) => {
-        numberButton.addEventListener('click', (event) => {
-            if (calculationIsComplete)
-            {
-                displayMessage.textContent = '';
-                calculationIsComplete = false;
-            }
-
-            displayMessage.textContent += event.target.textContent;
-
-            displayMessageArray = displayMessage.textContent.trimEnd().split(' ');
-
-            enableOperatorButtons(operatorButtons);
-
-            // Only enable equals button when an operation contains both operands and operator
-            if (displayMessageArray.length >= 3)
-            {
-                equalsButton.disabled = false;
-            }
-        });
-    });
-
-    operatorButtons.forEach((operatorButton) => {
-        operatorButton.disabled = true;
-
-        operatorButton.addEventListener('click', (event) => {
-            displayMessage.textContent += ' ' + event.target.textContent + ' ';
-
-            disableOperatorButtons(operatorButtons);
-
-            displayMessageArray = displayMessage.textContent.trimEnd().split(' ');
-
-            // If the user has entered more than one operator in a calculation, simulate
-            // the clicking of the "=" button, and use the result of the calculation as the
-            // first operand of a new calculation, adding to it the second operator entered
-            if (displayMessageArray.length > 3)
-            {
-                equalsButton.dispatchEvent(new Event('click'));
-
-                // Only add the operator to the display if the display does not contain a letter.
-                // This prevents the user from creating a calculation after they try to divide
-                // by 0, and a message appears on the display
-                if (!displayMessage.textContent.match(/[a-z]/))
-                {
-                    displayMessage.textContent += ' ' + event.target.textContent + ' ';
-                    calculationIsComplete = false;
-                }
-            }
-            else
-            {
-                calculationIsComplete = false;
-            }
-
-            // If an operator is used, that means the user is currently entering the second
-            // operand, so the decimal button should be enabled again if it had been previously
-            // disabled with the first operand
-            decimalButton.disabled = false;
-        });
-    });
-
-    equalsButton.addEventListener('click', () => {
-        const displayArray = displayMessage.textContent.split(' ');
-        const firstOperand = Number(displayArray[0]);
-        const operator = displayArray[1];
-        const secondOperand = Number(displayArray[2]);
-
-        displayMessage.textContent = '';
-
-        displayMessage.textContent = calculator.operate(operator, firstOperand, secondOperand);
-
-        equalsButton.disabled = true;
-        calculationIsComplete = true;
-        decimalButton.disabled = false;
-
-        // If the user previously tried to divide by 0, disable the operator buttons to prevent
-        // an error in calculation
-        if (displayMessage.textContent.match(/[a-z]/))
-        {
-            disableOperatorButtons(operatorButtons);
-        }
-        // Round answers with long decimals up to 4 decimal places or 4 significant figures
-        // to avoid overflow of the display
-        else if (!Number.isInteger(Number(displayMessage.textContent)))
-        {
-            if (Number(displayMessage.textContent) > 1)
-            {
-                displayMessage.textContent = Number(displayMessage.textContent).toFixed(4) * 1;
-            }
-            else
-            {
-                displayMessage.textContent = Number(displayMessage.textContent).toPrecision(4) * 1;
-            }
-        }
-    });
-
-    clearButton.addEventListener('click', () => {
-        displayMessage.textContent = '';
-        disableOperatorButtons(operatorButtons);
-        equalsButton.disabled = true;
-        decimalButton.disabled = false;
-    });
-
-    decimalButton.addEventListener('click', (event) => {
-        if (calculationIsComplete)
-        {
-            displayMessage.textContent = '';
-            calculationIsComplete = false;
-        }
-
-        displayMessage.textContent += event.target.textContent;
-
-        // Only allow one decimal point to be used per operand
-        decimalButton.disabled = true;
-
-        disableOperatorButtons(operatorButtons);
-    });
 
     backspaceButton.addEventListener('click', () => {
         if (displayMessage.textContent === '') return;
 
-        if (calculationIsComplete)
+        if (calculatorElements.calculationIsComplete)
         {
             displayMessage.textContent = '';
             return;
@@ -224,12 +239,43 @@ function addFunctionalityToButtons()
 
         let displayString = displayMessage.textContent;
 
-        deleteCharactersFromDisplay(displayString, displayMessage, decimalButton);
+        deleteCharactersFromDisplay(displayString, displayMessage, calculatorElements.decimalButton);
 
         let displayArray = displayMessage.textContent.trimEnd().split(' ');
 
-        enableOrDisableButtons(displayArray, operatorButtons, decimalButton, equalsButton);
+        enableOrDisableButtons(displayArray, calculatorElements.operatorButtons,
+                                calculatorElements.decimalButton, calculatorElements.equalsButton);
     });
+}
+
+function addFunctionalityToButtons()
+{
+    // Calculator elements that need to be accessed in more than one button function
+    const calculatorElements = {
+        operatorButtons: document.querySelectorAll('.operator-button'),
+        equalsButton: document.querySelector('.button-equals'),
+        decimalButton: document.querySelector('.button-decimal'),
+        calculationIsComplete: false,
+    }
+
+    // Declared outside of calculatorElements object to improve readability of code since
+    // it is used a lot throughout the functions. This way, I can avoid needing to access
+    // the calculatorElements each time I need to access the displayMessage
+    const displayMessage = document.querySelector('.display-message');
+
+    calculatorElements.equalsButton.disabled = true;
+
+    addFunctionalityToNumberButtons(calculatorElements, displayMessage);
+
+    addFunctionalityToOperatorButtons(calculatorElements, displayMessage);
+
+    addFunctionalityToEqualsButton(calculatorElements, displayMessage);
+
+    addFunctionalityToClearButton(calculatorElements, displayMessage);
+
+    addFunctionalityToDecimalButton(calculatorElements, displayMessage);
+
+    addFunctionalityToBackspaceButton(calculatorElements, displayMessage);
 }
 
 addFunctionalityToButtons();
